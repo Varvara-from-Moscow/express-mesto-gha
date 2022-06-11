31e5e21fcc6bec6f151d8dd419915afec7f8c347
Varvara Korableva <v.korableva1990@yandex.ru>
2022-06-11 11:57:42 +0300

    chanched after first test

--- controllers/users.js
+++ controllers/users.js
@@ -8,6 +8,12 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
+    .then((user) => {
+      if (!user) {
+        return res.status(NOT_FOUND_ERROR).send({ message: 'Ошибка, не правильно заполнены поля формы' });
+      }
+      return res.send({ data: user });
+    })
     .catch((err) => {
       if (err.name === 'CastError') {
         return res.status(BAD_REQUEST_ERROR).send({ message: 'Ошибка, некорректные данные в запросe' });
